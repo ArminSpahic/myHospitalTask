8533ad0608fcc1ffb3c19a2efae959aa383edbcf
ArminSpahic <armin1spahic@gmail.com>
2018-08-15 23:18:23 +0200

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Armin Spahic on 15/08/2018.
+//  Copyright © 2018 Armin Spahic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
